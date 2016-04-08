21c86f013c50dc49d3efb3c40e3c16547e14a324
nico <nico@debian.dom>
2016-04-08 09:45:42 -0300

    Se agrego servicios de cargas

---
+++ dom/src/main/java/dominio/servicio/CargaServicio.java
@@ -0,0 +1,81 @@
+package dominio.servicio;
+
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.annotation.DomainServiceLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.ParameterLayout;
+import dominio.dom.Computadora;
+import dominio.dom.Director;
+import dominio.dom.Sector;
+
+@DomainService
+@DomainServiceLayout(menuOrder = "2", named= "Carga de datos")
+public class CargaServicio
+{
+	    @MemberOrder(sequence = "1")
+		public Computadora cargarComputadora
+		(
+				 @ParameterLayout(named="Numero de serie")final String nSerie,
+				 @ParameterLayout(named="Nombre")final String nombre,
+				 @ParameterLayout(named="Detalle")final String detalle,
+				 @ParameterLayout(named="Sector")final Sector sector,
+				 @ParameterLayout(named="CPU")final String cpu,
+				 @ParameterLayout(named="Ram")final String ram,
+				 @ParameterLayout(named="Disco")final String disco,
+				 @ParameterLayout(named="IP")final String ip,
+				 @ParameterLayout(named="Team viewer")final String teamViewer
+		)
+		{
+			final Computadora c= container.newTransientInstance(Computadora.class);
+			c.setCpu(cpu);
+			c.setDetalle(detalle);
+			c.setDisco(disco);
+			c.setIp(ip);
+			c.setnSerie(nSerie);
+			c.setNombre(nombre);
+			c.setRam(ram);
+			c.setTeamViewer(teamViewer);
+			c.setSector(sector);
+			container.persistIfNotAlready(c);
+			return c;
+		}
+	 
+	    @MemberOrder(sequence = "2")
+	 	public Director cargarDirector
+	 	(
+	 			@ParameterLayout(named="Nombre") final String nombre,
+	 			@ParameterLayout(named="Numero") final String numero,
+	 			@ParameterLayout(named="Email") final String email
+	 	)
+	    {
+	    	final Director d = container.newTransientInstance(Director.class);
+	    	d.setEmail(email);
+	    	d.setNombre(nombre);
+	    	d.setNumero(numero);
+	    	container.persistIfNotAlready(d);
+	 		return d;
+	 	}
+	    
+	    @MemberOrder(sequence = "3")
+	    public Sector cargarSector
+	    (
+	    		@ParameterLayout(named="Nombre") final String nombre,
+	    		@ParameterLayout(named="Director") final Director director,
+	    		@ParameterLayout(named="Numero") final String numero,
+	    		@ParameterLayout(named="Email") final String email
+	    		)
+	    {
+	    	final Sector s= container.newTransientInstance(Sector.class);
+	    	s.setDirector(director);
+	    	s.setEmail(email);
+	    	s.setNombre(nombre);
+	    	s.setNumero(numero);
+	    	container.persistIfNotAlready(s);
+	    	return s;
+	    }
+	 
+	 
+	   @javax.inject.Inject 
+	    DomainObjectContainer container;
+}
